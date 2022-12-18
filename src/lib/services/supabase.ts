523d5033ebd47877d5985d726986b4ec11a557d8
Sean <mr.seangrover@icloud.com>
2022-12-18 08:27:41 +0800

    updates

--- src/lib/services/supabase.ts
+++ src/lib/services/supabase.ts
@@ -1,4 +1,6 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit'
 import { env } from '$env/dynamic/public'
 
+console.log(env)
+
 export const supabase = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY)
